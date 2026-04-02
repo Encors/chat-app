c920c6f9b418adb94660bd050d58afcea4f915fc
Encors <cheperish@mail.ru>
2026-04-02 22:24:57 +0500

    fix: стили

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -3,10 +3,10 @@ import { inject } from '@angular/core';
 import { AuthService } from '@app/shared/services/auth.service';
 
 export const authGuard: CanActivateFn = () => {
-  const authStore = inject(AuthService);
+  const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authStore.isAuthenticated()) {
+  if (authService.isAuthenticated()) {
     return true;
   }
 
